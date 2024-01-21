f16c97e95c7e52b415a8801f5800859a70e3ac8a
chiemezie1 <chiemezieagbo1@gmail.com>
2024-01-21 07:54:37 +0100

    div.c

--- execute.c
+++ execute.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 /**
-* execute - executes the opcode
+* execute - executes the opcode function corresponding to the opcode
 * @stack: head linked list - stack
 * @counter: line_counter
 * @file: poiner to monty file
